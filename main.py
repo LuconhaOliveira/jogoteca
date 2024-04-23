664080dbdc4c236a847df7883b8c25767e04f531
Lucas Silva <lucaas.oliveira0254@gmail.com>
2024-04-23 08:52:28 -0300

    fix: parte grafica

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import os
+
+os.system("cls")
 print("""-----------------
 SEJA BEM-VINDE A
   🎮JOGOTECA🎮
 -----------------
-Selecione o jogo:
-> 1. Jogo da forca
-> 2. Jogo da velha
-> 3. Jogo da adivinhação
-> 4. Jogo da tabuada""")
+""")
 
 while True:
-    idade = int(input("qual sua idade"))
+    idade = int(input("qual sua idade "))
     if idade<18:
         print("some daqui meo")
         exit()
     elif idade > 120:
         print("não minta idade")
     else:
-        print("venha jogar!!!")
+        print("""Selecione o jogo:
+> 1. Jogo da forca
+> 2. Jogo da velha
+> 3. Jogo da adivinhação
+> 4. Jogo da tabuada
+""")
         break
