76843f2d11b5bbb559e6b94a529df2b8e8b3b0d3
Lucas Silva <lucaas.oliveira0254@gmail.com>
2024-04-23 08:30:32 -0300

    Add: verificação de idade

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ Selecione o jogo:
 > 2. Jogo da velha
 > 3. Jogo da adivinhação
 > 4. Jogo da tabuada""")
+
+while True:
+    idade = int(input("qual sua idade"))
+    if idade<18:
+        print("some daqui meo")
+        exit()
+    elif idade > 120:
+        print("não minta idade")
+    else:
+        print("venha jogar!!!")
+        break
