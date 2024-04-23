857fa164702f2f7e17f20d7b266fec23728462dc
Lucas Silva <lucaas.oliveira0254@gmail.com>
2024-04-23 08:03:39 -0300

    Add: tabuada pronto para jogar

--- jogo_tabuada.py
+++ jogo_tabuada.py
@@ -1,3 +1,14 @@
 import random
-n1 = random.randint(0,10)
-n2 = random.randint(0,10)
+
+pontos = 0
+while True:
+    n1 = random.randint(0,10)
+    n2 = random.randint(0,10)
+    resp = int(input(f"qual o resultado de {n1} X {n2} "))
+    if resp == n1*n2:
+        print("acertou")
+        pontos+=1
+    else:
+        print("errou")
+        break
+print(f"parabens você fez {pontos} pontos")
